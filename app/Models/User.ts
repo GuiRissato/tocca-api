ca6b16137d23a87ba48c57e8ddc07381dae09e8b
Guilherme Rissato <52382877+GuiRissato@users.noreply.github.com>
2023-09-12 17:45:20 -0300

    create: created api for tocca

---
+++ app/Models/User.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public user_name: string
+
+  @column({serializeAs: null})
+  public password: string
+
+  @column()
+  public name: string
+
+  @column()
+  public age: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
