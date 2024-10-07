4605729113f21d2cd64661bc6e42d556473045e9
GuiRissato <guilherme_andrade_rissato@hotmail.com>
2024-10-07 20:50:31 -0300

    create company controller and service

---
+++ src/company/company.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+import { Company } from './entities/company.entity';
+
+@Injectable()
+export class CompanyService {
+  constructor(
+    @InjectRepository(Company) private readonly repository: Repository<Company>,
+  ) {}
+  create(createCompanyDto: CreateCompanyDto): Promise<Company | object> {
+    try {
+      const company = this.repository.create(createCompanyDto);
+      return this.repository.save(company);
+    } catch (error) {
+      console.log('error creating new company', error.message);
+    }
+  }
+
+  findAll(): Promise<Company[]> {
+    try {
+      const allCompanies = this.repository.find();
+      return allCompanies;
+    } catch (error) {
+      console.log('error finding all companies', error.message);
+    }
+  }
+
+  findOne(id: number): Promise<Company> {
+    try {
+      const company = this.repository.findOne({ where: { id } });
+      return company;
+    } catch (error) {
+      console.log('error finding company', error.message);
+    }
+  }
+
+  async update(
+    id: number,
+    updateCompanyDto: UpdateCompanyDto,
+  ): Promise<Company> {
+    try {
+      const company = await this.repository.preload({
+        id: id,
+        ...updateCompanyDto,
+      });
+      if (!company) {
+        throw new NotFoundException(`Company ${id} not found`);
+      }
+      return this.repository.save(company);
+    } catch (error) {
+      console.log('error updating company', error.message);
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      const company = await this.findOne(id);
+      return this.repository.remove(company);
+    } catch (error) {
+      console.log('error deleting company', error.message);
+    }
+  }
+}
