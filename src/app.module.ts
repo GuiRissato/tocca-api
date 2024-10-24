70b7d1f236d1aabe2c3697bfe5371a7983ed84d6
GuiRissato <guilherme_andrade_rissato@hotmail.com>
2024-10-23 21:08:02 -0300

    fix dev start app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './companies/company.module';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { OkrProjectsModule } from './okr_projects/okr_projects.module';
-import { CompanyController } from './companies/company.controller';
-import { OkrProjectsController } from './okr_projects/okr_projects.controller';
-import { UsersService } from './users/users.service';
-import { CompanyService } from './companies/company.service';
-import { OkrProjectsService } from './okr_projects/okr_projects.service';
 import { ObjectivesModule } from './objectives/objectives.module';
 import { KeyResultsModule } from './key_results/key_results.module';
-import { ColumnsModule } from './columns/columns.module';
-import { ColumnsController } from './columns/columns.controller';
 import { ColumnsKeyResultModule } from './columns_key_result/columns_key_result.module';
 
 @Module({
@@ -36,22 +28,9 @@ import { ColumnsKeyResultModule } from './columns_key_result/columns_key_result.
     OkrProjectsModule,
     ObjectivesModule,
     KeyResultsModule,
-    ColumnsModule,
     ColumnsKeyResultModule,
   ],
-  controllers: [
-    AppController,
-    UsersController,
-    CompanyController,
-    OkrProjectsController,
-    ColumnsController,
-  ],
-  providers: [
-    AppService,
-    UsersService,
-    CompanyService,
-    OkrProjectsService,
-    ColumnsController,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
