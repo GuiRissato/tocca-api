0500024f187a9a7af4746d6aa1abb07e2f09e773
GuiRissato <guilherme_andrade_rissato@hotmail.com>
2024-11-21 23:20:19 -0300

    fix jwt info for users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity'; // Assuming you have a User entity
+import { UsersController } from './users.controller'; // Assuming you have a UsersController
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController, JwtStrategy],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
